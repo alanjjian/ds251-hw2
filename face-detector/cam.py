91b1d59e36acc8041d39e3ecd6ed047c1052ca69
alanjjian <alanjian131@berkeley.edu>
2023-01-27 01:13:00 -0500

    working on face detect

---
+++ face-detector/cam.py
@@ -0,0 +1,31 @@
+# cam.py
+# this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
+import numpy as np
+import cv2
+
+
+# the index depends on your camera setup and which one is your USB camera.
+# you may need to change to 1 or 2 depending on your machine.
+cap = cv2.VideoCapture(0) # with macOS and an iphone, this might be your iphone camera
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    print(frame)
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    
+    for (x,y,w,h) in faces:
+        print("detected!")
+
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
