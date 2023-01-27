7270d2f0a725727630e61cd210e33000e4247914
alanjjian <alanjian131@berkeley.edu>
2023-01-27 01:32:37 -0500

    imported paho-mqtt

--- face-detector/cam.py
+++ face-detector/cam.py
@@ -2,7 +2,7 @@
 # this is from https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 import numpy as np
 import cv2
-
+import paho.mqtt.client as mqtt
 
 # the index depends on your camera setup and which one is your USB camera.
 # you may need to change to 1 or 2 depending on your machine.
@@ -12,17 +12,18 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    print(frame)
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     
     for (x,y,w,h) in faces:
-        print("detected!")
+        face = gray[x:x+w, y:y+h]
+        rc, png = cv2.imencode('.png', face)
+        msg = png.tobytes()
 
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
