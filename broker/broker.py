ff86dcfdd32a45b9c8bbd686996ce83a7c98c6ef
alanjjian <alanjian131@berkeley.edu>
2023-02-02 18:59:31 -0500

    working on broker-deployment

--- broker/broker.py
+++ broker/broker.py
@@ -1,7 +1,7 @@
 import paho.mqtt.client as mqtt
 
 
-LOCAL_MQTT_HOST="localhost"
+LOCAL_MQTT_HOST="face-detector-service"
 LOCAL_MQTT_INBOUND = 1883
 LOCAL_MQTT_OUTBOUND = 1884
 LOCAL_MQTT_TOPIC= "test_topic"
@@ -15,7 +15,7 @@ def on_message(client,userdata, msg):
     print("message received: ",str(msg.payload.decode("utf-8")))
     # if we wanted to re-publish this message, something like this should work
     msg = msg.payload
-    remote_mqttclient.publish(LOCAL_MQTT_TOPIC, payload=msg, qos=0, retain=False)
+    # remote_mqttclient.publish(LOCAL_MQTT_TOPIC, payload=msg, qos=0, retain=False)
   except:
     print("Unexpected error:", sys.exc_info()[0])
 
@@ -23,9 +23,9 @@ local_mqttclient = mqtt.Client()
 local_mqttclient.on_connect = on_connect_local
 local_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_INBOUND, 60)
 
-remote_mqttclient = mqtt.Client()
-remote_mqttclient.on_connect = on_connect_local
-remote_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_OUTBOUND, 60)
+# remote_mqttclient = mqtt.Client()
+# remote_mqttclient.on_connect = on_connect_local
+# remote_mqttclient.connect(LOCAL_MQTT_HOST, LOCAL_MQTT_OUTBOUND, 60)
 
 local_mqttclient.on_message = on_message
 
