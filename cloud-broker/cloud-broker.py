7d4b46c03e497b9000728ec6ca6db9ff987f07a5
alanjjian <alanjian131@berkeley.edu>
2023-02-14 05:09:02 -0500

    locals are talking

--- cloud-broker/cloud-broker.py
+++ cloud-broker/cloud-broker.py
@@ -1,11 +1,15 @@
 import paho.mqtt.client as mqtt
 import sys
 import base64
+import boto3
+import numpy as np
+import matplotlib.pyplot as plt
 
 LOCAL_MQTT_HOST="localhost"
-LOCAL_MQTT_INBOUND = 1883
-LOCAL_MQTT_OUTBOUND = 1884
+LOCAL_MQTT_INBOUND = 1884
+LOCAL_MQTT_OUTBOUND = 1885
 LOCAL_MQTT_TOPIC= "test_topic"
+prefix = 'face-images'
 
 def on_connect_local(client, userdata, flags, rc):
         print("connected to local broker with rc: " + str(rc))
@@ -15,7 +19,14 @@ def on_message(client,userdata, msg):
   try:
     print("message received: ",str(base64.b64decode(msg.payload)))
     # if we wanted to re-publish this message, something like this should work
-    msg = msg.payload
+    decoded_img = base64.b64.decode(msg.payload)
+    img_arr = np.array(decoded_img)
+    img = plt.imshow(decoded_img, interpolation='nearest')
+    plt.savefig("img.png")
+    filename = str(int(np.random.random() * 10000000000)) + ".png"
+    boto3.Session().resource('s3').Bucket(bucket).Object(
+            os.path.join(prefix, filename).upload_file("img.png"))   
+
     remote_mqttclient.publish(LOCAL_MQTT_TOPIC, payload=msg, qos=0, retain=False)
   except Exception as e:
     print(e)
